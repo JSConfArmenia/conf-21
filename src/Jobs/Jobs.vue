127c517042443b434bec627531bf7bf88474376b
Varser <varserzakaryan@gmail.com>
2021-06-14 13:37:59 +0400

    add targets to new tab

--- src/Jobs/Jobs.vue
+++ src/Jobs/Jobs.vue
@@ -4,139 +4,139 @@
 
     <div class="row Vacancies">
       <div class="col-md-4">
-        <a href="https://www.simplytechnologies.net/career/frontend-developer">
+        <a href="https://www.simplytechnologies.net/career/frontend-developer" target="_blank">
             <h3 class="Vacancy">Senior Front-end Developer</h3>
             <p class="Company">Simply Technologies</p>
         </a>
-        <a href="https://www.simplytechnologies.net/careers/nodejs-developer">
+        <a href="https://www.simplytechnologies.net/careers/nodejs-developer" target="_blank">
             <h3 class="Vacancy">Node.js Developer</h3>
             <p class="Company">Simply Technologies</p>
         </a>
-        <a href="https://hexact.io/career">
+        <a href="https://hexact.io/career" target="_blank">
             <h3 class="Vacancy">Node.js Developer</h3>
             <p class="Company">Hexact, Inc.</p>
         </a>
-        <a href="https://brainstormtech.io/careers/vacancy-lead-front-end-developer">
+        <a href="https://brainstormtech.io/careers/vacancy-lead-front-end-developer" target="_blank">
             <h3 class="Vacancy">Lead Frontend Developer</h3>
             <p class="Company">Brainstorm Technologies</p>
         </a>
-        <a href="https://jobs.smartrecruiters.com/SuperAnnotate/743999751131041-senior-frontend-software-engineer">
+        <a href="https://jobs.smartrecruiters.com/SuperAnnotate/743999751131041-senior-frontend-software-engineer" target="_blank">
             <h3 class="Vacancy">Senior Frontend Engineer</h3>
             <p class="Company">SuperAnnotate</p>
         </a>
-        <a href="https://jobs.krisp.ai/13632190e5dd4094bd39a0fa539f05f4">
+        <a href="https://jobs.krisp.ai/13632190e5dd4094bd39a0fa539f05f4" target="_blank">
             <h3 class="Vacancy">Senior Frontend Engineer II</h3>
             <p class="Company">Krisp</p>
         </a>
-        <a href="https://sdc.am/career/6075e592c04ed00019997950">
+        <a href="https://sdc.am/career/6075e592c04ed00019997950" target="_blank">
             <h3 class="Vacancy">JavaScript Backend Engineer</h3>
             <p class="Company">SDC</p>
         </a>
-        <a href="https://fluxtech.me/careers/?tab=senior-node-js-developer">
+        <a href="https://fluxtech.me/careers/?tab=senior-node-js-developer" target="_blank">
             <h3 class="Vacancy">Senior Node.js Developer</h3>
             <p class="Company">Flux Technologies</p>
         </a>
-        <a href="https://adobe.wd5.myworkdayjobs.com/external_experienced/job/Yerevan/Software-Engineer-ll_R109160">
+        <a href="https://adobe.wd5.myworkdayjobs.com/external_experienced/job/Yerevan/Software-Engineer-ll_R109160" target="_blank">
             <h3 class="Vacancy">Software Engineer /React/</h3>
             <p class="Company">Adobe</p>
         </a>
-        <a href="hhttps://adobe.wd5.myworkdayjobs.com/external_experienced/job/Yerevan/Software-Engineer-ll_R109159">
+        <a href="hhttps://adobe.wd5.myworkdayjobs.com/external_experienced/job/Yerevan/Software-Engineer-ll_R109159" target="_blank">
             <h3 class="Vacancy">Software Engineer /Node.js/</h3>
             <p class="Company">Adobe</p>
         </a>
-        <a href="https://www.renderforest.com/careers/node-js-developer">
+        <a href="https://www.renderforest.com/careers/node-js-developer" target="_blank">
             <h3 class="Vacancy">Node.js Developer</h3>
             <p class="Company">Renderforest</p>
         </a>
-        <a href="https://www.renderforest.com/careers/reactjs-engineer">
+        <a href="https://www.renderforest.com/careers/reactjs-engineer" target="_blank">
             <h3 class="Vacancy">React.js Developer</h3>
             <p class="Company">Renderforest</p>
         </a>
       </div>
       <div class="col-md-4">
-        <a href="https://www.simplytechnologies.net/careers/react-native-developer">
+        <a href="https://www.simplytechnologies.net/careers/react-native-developer" target="_blank">
             <h3 class="Vacancy">Mid React Native Developer</h3>
             <p class="Company">Simply Technologies</p>
         </a>
-        <a href="https://hexact.io/career">
+        <a href="https://hexact.io/career" target="_blank">
             <h3 class="Vacancy">React.js Developer</h3>
             <p class="Company">Hexact, Inc.</p>
         </a>
-        <a href="https://jobs.smartrecruiters.com/SuperAnnotate/743999742519834-senior-backend-software-engineer">
+        <a href="https://jobs.smartrecruiters.com/SuperAnnotate/743999742519834-senior-backend-software-engineer" target="_blank">
             <h3 class="Vacancy">Senior Backend Engineer</h3>
             <p class="Company">SuperAnnotate</p>
         </a>
-        <a href="https://brainstormtech.io/careers/careers-react-native-react">
+        <a href="https://brainstormtech.io/careers/careers-react-native-react" target="_blank">
             <h3 class="Vacancy">React Native Developer</h3>
             <p class="Company">Brainstorm Technologies</p>
         </a>
-        <a href="https://jobs.krisp.ai/f00548600b3e454e8bf8ff1089faede8">
+        <a href="https://jobs.krisp.ai/f00548600b3e454e8bf8ff1089faede8" target="_blank">
             <h3 class="Vacancy">Web Architect</h3>
             <p class="Company">Krisp</p>
         </a>
-        <a href="https://sdc.am/career/60848a750b80670019ad682c">
+        <a href="https://sdc.am/career/60848a750b80670019ad682c" target="_blank">
             <h3 class="Vacancy">JavaScript Frontend Engineer</h3>
             <p class="Company">SDC</p>
         </a>
-        <a href="https://fluxtech.me/careers/?tab=vue-js-developer">
+        <a href="https://fluxtech.me/careers/?tab=vue-js-developer" target="_blank">
             <h3 class="Vacancy">Vue.js Developer</h3>
             <p class="Company">Flux Technologies</p>
         </a>
-        <a href="https://fluxtech.me/careers/?tab=senior-java-script-developer">
+        <a href="https://fluxtech.me/careers/?tab=senior-java-script-developer" target="_blank">
             <h3 class="Vacancy">Lead JS Developer</h3>
             <p class="Company">Flux Technologies</p>
         </a>
-        <a href="https://adobe.wd5.myworkdayjobs.com/external_experienced/job/Yerevan/Senior-Software-Engineer---Developer-Productivity_R110213-1">
+        <a href="https://adobe.wd5.myworkdayjobs.com/external_experienced/job/Yerevan/Senior-Software-Engineer---Developer-Productivity_R110213-1" target="_blank">
             <h3 class="Vacancy">Senior Software Engineer - Developer Productivity /Node.js/ </h3>
             <p class="Company">Adobe</p>
         </a>
-        <a href="https://adobe.wd5.myworkdayjobs.com/external_experienced/job/Yerevan/Software-Engineer---Developer-Productivity_R111578-1">
+        <a href="https://adobe.wd5.myworkdayjobs.com/external_experienced/job/Yerevan/Software-Engineer---Developer-Productivity_R111578-1" target="_blank">
             <h3 class="Vacancy">Senior Software Engineer - Developer Productivity /Node.js/ </h3>
             <p class="Company">Adobe</p>
         </a>
-        <a href="https://adobe.wd5.myworkdayjobs.com/external_experienced/job/Yerevan/Senior-Software-Engineer_R109154-1">
+        <a href="https://adobe.wd5.myworkdayjobs.com/external_experienced/job/Yerevan/Senior-Software-Engineer_R109154-1" target="_blank">
             <h3 class="Vacancy">Senior Front-end Software Engineer /React/</h3>
             <p class="Company">Adobe</p>
         </a>
       </div>
       <div class="col-md-4">
-        <a href="https://www.simplytechnologies.net/careers/javascript-developer">
+        <a href="https://www.simplytechnologies.net/careers/javascript-developer" target="_blank">
             <h3 class="Vacancy">Full-Stack JS Developer</h3>
             <p class="Company">Simply Technologies</p>
         </a>
-        <a href="https://www.linkedin.com/jobs/view/2533543354/?refId=rabGHr0icxYLCViR948gNg%3D%3D&trackingId=ji4zFY%2FNGhoj9RZGNALmVQ%3D%3D&trk=d_flagship3_company">
+        <a href="https://www.linkedin.com/jobs/view/2533543354/?refId=rabGHr0icxYLCViR948gNg%3D%3D&trackingId=ji4zFY%2FNGhoj9RZGNALmVQ%3D%3D&trk=d_flagship3_company" target="_blank">
             <h3 class="Vacancy">Senior Front-end Engineer</h3>
             <p class="Company">MentorcliQ</p>
         </a>
-        <a href="https://brainstormtech.io/careers/node-js-developer">
+        <a href="https://brainstormtech.io/careers/node-js-developer" target="_blank">
           <h3 class="Vacancy">Node.js Developer</h3>
           <p class="Company">Brainstorm Technologies</p>
         </a>
-        <a href="https://jobs.krisp.ai/3f8a35e3a4c24addaaccfecfdf24b57d">
+        <a href="https://jobs.krisp.ai/3f8a35e3a4c24addaaccfecfdf24b57d" target="_blank">
             <h3 class="Vacancy">Senior Backend Engineer II</h3>
             <p class="Company">Krisp</p>
         </a>
-        <a href="https://cauldron.app/jobs/60afa51c5ec67a4bb266ed03?c=1">
+        <a href="https://cauldron.app/jobs/60afa51c5ec67a4bb266ed03?c=1" target="_blank">
             <h3 class="Vacancy">Node.js Developer</h3>
             <p class="Company">CoinStats</p>
         </a>
-        <a href="https://cauldron.app/jobs/60afa8685ec67a4bb266ed0d?c=1">
+        <a href="https://cauldron.app/jobs/60afa8685ec67a4bb266ed0d?c=1" target="_blank">
             <h3 class="Vacancy">React.js Developer</h3>
             <p class="Company">CoinStats</p>
         </a>
-        <a href="https://fluxtech.me/careers/?tab=angular-developer">
+        <a href="https://fluxtech.me/careers/?tab=angular-developer" target="_blank">
             <h3 class="Vacancy">Angular 2.X+ Developer</h3>
             <p class="Company">Flux Technologies</p>
         </a>
-        <a href="https://boards.greenhouse.io/vineti/jobs/4432956003">
+        <a href="https://boards.greenhouse.io/vineti/jobs/4432956003" target="_blank">
             <h3 class="Vacancy">Application Support Engineer</h3>
             <p class="Company">Vineti</p>
         </a>
-        <a href="https://boards.greenhouse.io/vineti/jobs/4485746003">
+        <a href="https://boards.greenhouse.io/vineti/jobs/4485746003" target="_blank">
             <h3 class="Vacancy">Senior Manager, Software Engineering</h3>
             <p class="Company">Vineti</p>
         </a>
-        <a href="https://boards.greenhouse.io/vineti/jobs/4404443003">
+        <a href="https://boards.greenhouse.io/vineti/jobs/4404443003" target="_blank">
             <h3 class="Vacancy">Senior Software Engineer</h3>
             <p class="Company">Vineti</p>
         </a>
