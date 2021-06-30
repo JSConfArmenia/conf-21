2cbd314222be492e5aabb12330b3f1ef799caa87
Varser <varserzakaryan@gmail.com>
2021-07-01 02:46:23 +0400

    added new jobs and rearrange the section according the tech stack

--- src/Jobs/Jobs.vue
+++ src/Jobs/Jobs.vue
@@ -4,17 +4,10 @@
 
     <div class="row Vacancies">
       <div class="col-md-4">
+        <!-- Frontend -->
         <a href="https://www.simplytechnologies.net/career/frontend-developer" target="_blank">
             <h3 class="Vacancy">Senior Front-end Developer</h3>
             <p class="Company">Simply Technologies</p>
-        </a>
-        <a href="https://www.simplytechnologies.net/careers/nodejs-developer" target="_blank">
-            <h3 class="Vacancy">Node.js Developer</h3>
-            <p class="Company">Simply Technologies</p>
-        </a>
-        <a href="https://hexact.io/career" target="_blank">
-            <h3 class="Vacancy">Node.js Developer</h3>
-            <p class="Company">Hexact, Inc.</p>
         </a>
         <a href="https://brainstormtech.io/careers/vacancy-lead-front-end-developer" target="_blank">
             <h3 class="Vacancy">Lead Frontend Developer</h3>
@@ -24,55 +17,17 @@
             <h3 class="Vacancy">Senior Frontend Engineer</h3>
             <p class="Company">SuperAnnotate</p>
         </a>
-        <a href="https://jobs.krisp.ai/b6a95a0c75b54c77bcc9028a3af4a2b6" target="_blank">
-            <h3 class="Vacancy">Senior Frontend Engineer II</h3>
-            <p class="Company">Krisp</p>
-        </a>
-        <a href="https://sdc.am/career/6075e592c04ed00019997950" target="_blank">
-            <h3 class="Vacancy">JavaScript Backend Engineer</h3>
-            <p class="Company">SDC</p>
-        </a>
-        <a href="https://fluxtech.me/careers/?tab=senior-node-js-developer" target="_blank">
-            <h3 class="Vacancy">Senior Node.js Developer</h3>
-            <p class="Company">Flux Technologies</p>
-        </a>
-        <a href="https://adobe.wd5.myworkdayjobs.com/external_experienced/job/Yerevan/Software-Engineer-ll_R109160" target="_blank">
-            <h3 class="Vacancy">Software Engineer /React/</h3>
-            <p class="Company">Adobe</p>
-        </a>
-        <a href="hhttps://adobe.wd5.myworkdayjobs.com/external_experienced/job/Yerevan/Software-Engineer-ll_R109159" target="_blank">
-            <h3 class="Vacancy">Software Engineer /Node.js/</h3>
-            <p class="Company">Adobe</p>
-        </a>
-        <a href="https://www.renderforest.com/careers/node-js-developer" target="_blank">
-            <h3 class="Vacancy">Node.js Developer</h3>
-            <p class="Company">Renderforest</p>
-        </a>
-        <a href="https://www.renderforest.com/careers/reactjs-engineer" target="_blank">
-            <h3 class="Vacancy">React.js Developer</h3>
-            <p class="Company">Renderforest</p>
-        </a>
-      </div>
-      <div class="col-md-4">
-        <a href="https://www.simplytechnologies.net/careers/react-native-developer" target="_blank">
-            <h3 class="Vacancy">Mid React Native Developer</h3>
-            <p class="Company">Simply Technologies</p>
-        </a>
         <a href="https://hexact.io/career" target="_blank">
             <h3 class="Vacancy">React.js Developer</h3>
             <p class="Company">Hexact, Inc.</p>
         </a>
-        <a href="https://jobs.smartrecruiters.com/SuperAnnotate/743999742519834-senior-backend-software-engineer" target="_blank">
-            <h3 class="Vacancy">Senior Backend Engineer</h3>
-            <p class="Company">SuperAnnotate</p>
+        <a href="https://www.linkedin.com/jobs/view/2533543354/?refId=rabGHr0icxYLCViR948gNg%3D%3D&trackingId=ji4zFY%2FNGhoj9RZGNALmVQ%3D%3D&trk=d_flagship3_company" target="_blank">
+            <h3 class="Vacancy">Senior Front-end Engineer</h3>
+            <p class="Company">MentorcliQ</p>
         </a>
-        <a href="https://brainstormtech.io/careers/careers-react-native-react" target="_blank">
-            <h3 class="Vacancy">React Native Developer</h3>
-            <p class="Company">Brainstorm Technologies</p>
-        </a>
-        <a href="https://jobs.krisp.ai/f00548600b3e454e8bf8ff1089faede8" target="_blank">
-            <h3 class="Vacancy">Web Architect</h3>
-            <p class="Company">Krisp</p>
+        <a href="https://www.renderforest.com/careers/reactjs-engineer" target="_blank">
+            <h3 class="Vacancy">React.js Developer</h3>
+            <p class="Company">Renderforest</p>
         </a>
         <a href="https://sdc.am/career/60848a750b80670019ad682c" target="_blank">
             <h3 class="Vacancy">JavaScript Frontend Engineer</h3>
@@ -82,9 +37,99 @@
             <h3 class="Vacancy">Vue.js Developer</h3>
             <p class="Company">Flux Technologies</p>
         </a>
-        <a href="https://fluxtech.me/careers/?tab=senior-java-script-developer" target="_blank">
-            <h3 class="Vacancy">Lead JS Developer</h3>
+        <a href="https://cauldron.app/jobs/60afa8685ec67a4bb266ed0d?c=1" target="_blank">
+            <h3 class="Vacancy">React.js Developer</h3>
+            <p class="Company">CoinStats</p>
+        </a>
+        <a href="https://fluxtech.me/careers/?tab=angular-developer" target="_blank">
+            <h3 class="Vacancy">Angular 2.X+ Developer</h3>
             <p class="Company">Flux Technologies</p>
+        </a>
+        <a href="https://jobs.krisp.ai/b6a95a0c75b54c77bcc9028a3af4a2b6" target="_blank">
+            <h3 class="Vacancy">Senior Frontend Engineer II</h3>
+            <p class="Company">Krisp</p>
+        </a>
+        <a href="https://adobe.wd5.myworkdayjobs.com/external_experienced/job/Yerevan/Software-Engineer-ll_R109160" target="_blank">
+            <h3 class="Vacancy">Software Engineer /React/</h3>
+            <p class="Company">Adobe</p>
+        </a>
+        <a href="https://adobe.wd5.myworkdayjobs.com/external_experienced/job/Yerevan/Senior-Software-Engineer_R109154-1" target="_blank">
+            <h3 class="Vacancy">Senior Front-end Software Engineer /React/</h3>
+            <p class="Company">Adobe</p>
+        </a>
+        <a href="https://adobe.wd5.myworkdayjobs.com/external_experienced/job/Yerevan/Senior-Software-Engineer_R109154-1" target="_blank">
+            <h3 class="Vacancy">Senior Front-end Software Engineer /React/</h3>
+            <p class="Company">Adobe</p>
+        </a>
+        <a href="https://www.digitain.com/vacancy/javascript-developer/" target="_blank">
+            <h3 class="Vacancy">JavaScript Developer</h3>
+            <p class="Company">Digitain</p>
+        </a>
+        <a href="https://jobs.dou.ua/companies/s-pro/vacancies/85542/" target="_blank">
+            <h3 class="Vacancy">Front-end (React+Next.JS) Developer (Remote)</h3>
+            <p class="Company">S-Pro</p>
+        </a>
+        <a href="https://www.epam.com/careers/job-listings/job.62173.senior-react-js-developer_armenia" target="_blank">
+            <h3 class="Vacancy">Senior React.JS Developer</h3>
+            <p class="Company">EPAM Systems</p>
+        </a>
+        <a href="https://www.epam.com/careers/job-listings/job.52131.senior-javascript-software-engineer-with-reactjs-redux_yerevan_armenia" target="_blank">
+            <h3 class="Vacancy">Senior Software Engineer (React.JS, Redux)</h3>
+            <p class="Company">EPAM Systems</p>
+        </a>
+      </div>
+      <div class="col-md-4">
+      <!-- Backend -->
+        <a href="https://www.simplytechnologies.net/careers/react-native-developer" target="_blank">
+            <h3 class="Vacancy">Mid React Native Developer</h3>
+            <p class="Company">Simply Technologies</p>
+        </a>
+        <a href="https://brainstormtech.io/careers/careers-react-native-react" target="_blank">
+            <h3 class="Vacancy">React Native Developer</h3>
+            <p class="Company">Brainstorm Technologies</p>
+        </a>
+        <a href="https://jobs.dou.ua/companies/s-pro/vacancies/91381/" target="_blank">
+            <h3 class="Vacancy">React Native Developer (Remote) </h3>
+            <p class="Company">S-Pro</p>
+        </a><a href="https://www.simplytechnologies.net/careers/nodejs-developer" target="_blank">
+            <h3 class="Vacancy">Node.js Developer</h3>
+            <p class="Company">Simply Technologies</p>
+        </a>
+        <a href="https://brainstormtech.io/careers/node-js-developer" target="_blank">
+          <h3 class="Vacancy">Node.js Developer</h3>
+          <p class="Company">Brainstorm Technologies</p>
+        </a>
+        <a href="https://hexact.io/career" target="_blank">
+            <h3 class="Vacancy">Node.js Developer</h3>
+            <p class="Company">Hexact, Inc.</p>
+        </a>
+        <a href="https://jobs.smartrecruiters.com/SuperAnnotate/743999742519834-senior-backend-software-engineer" target="_blank">
+            <h3 class="Vacancy">Senior Backend Engineer</h3>
+            <p class="Company">SuperAnnotate</p>
+        </a>
+        <a href="https://sdc.am/career/6075e592c04ed00019997950" target="_blank">
+            <h3 class="Vacancy">JavaScript Backend Engineer</h3>
+            <p class="Company">SDC</p>
+        </a>
+        <a href="https://fluxtech.me/careers/?tab=senior-node-js-developer" target="_blank">
+            <h3 class="Vacancy">Senior Node.js Developer</h3>
+            <p class="Company">Flux Technologies</p>
+        </a>
+        <a href="https://cauldron.app/jobs/60afa51c5ec67a4bb266ed03?c=1" target="_blank">
+            <h3 class="Vacancy">Node.js Developer</h3>
+            <p class="Company">CoinStats</p>
+        </a>
+        <a href="https://www.renderforest.com/careers/node-js-developer" target="_blank">
+            <h3 class="Vacancy">Node.js Developer</h3>
+            <p class="Company">Renderforest</p>
+        </a>
+        <a href="https://jobs.krisp.ai/3f8a35e3a4c24addaaccfecfdf24b57d" target="_blank">
+            <h3 class="Vacancy">Senior Backend Engineer II</h3>
+            <p class="Company">Krisp</p>
+        </a>
+        <a href="hhttps://adobe.wd5.myworkdayjobs.com/external_experienced/job/Yerevan/Software-Engineer-ll_R109159" target="_blank">
+            <h3 class="Vacancy">Software Engineer /Node.js/</h3>
+            <p class="Company">Adobe</p>
         </a>
         <a href="https://adobe.wd5.myworkdayjobs.com/external_experienced/job/Yerevan/Senior-Software-Engineer---Developer-Productivity_R110213-1" target="_blank">
             <h3 class="Vacancy">Senior Software Engineer - Developer Productivity /Node.js/ </h3>
@@ -94,39 +139,20 @@
             <h3 class="Vacancy">Senior Software Engineer - Developer Productivity /Node.js/ </h3>
             <p class="Company">Adobe</p>
         </a>
-        <a href="https://adobe.wd5.myworkdayjobs.com/external_experienced/job/Yerevan/Senior-Software-Engineer_R109154-1" target="_blank">
-            <h3 class="Vacancy">Senior Front-end Software Engineer /React/</h3>
-            <p class="Company">Adobe</p>
+        <a href="https://jobs.dou.ua/companies/s-pro/vacancies/151460/" target="_blank">
+            <h3 class="Vacancy">Backend Node.js Developer (Remote)</h3>
+            <p class="Company">S-Pro</p>
+        </a>
+        <a href="https://www.epam.com/careers/job-listings/job.56982.senior-node-js-engineer_yerevan_armenia" target="_blank">
+            <h3 class="Vacancy">Senior Node.js Engineer</h3>
+            <p class="Company">EPAM Systems</p>
         </a>
       </div>
       <div class="col-md-4">
+        <!-- Fullstack, Mobile & More -->
         <a href="https://www.simplytechnologies.net/careers/javascript-developer" target="_blank">
             <h3 class="Vacancy">Full-Stack JS Developer</h3>
             <p class="Company">Simply Technologies</p>
-        </a>
-        <a href="https://www.linkedin.com/jobs/view/2533543354/?refId=rabGHr0icxYLCViR948gNg%3D%3D&trackingId=ji4zFY%2FNGhoj9RZGNALmVQ%3D%3D&trk=d_flagship3_company" target="_blank">
-            <h3 class="Vacancy">Senior Front-end Engineer</h3>
-            <p class="Company">MentorcliQ</p>
-        </a>
-        <a href="https://brainstormtech.io/careers/node-js-developer" target="_blank">
-          <h3 class="Vacancy">Node.js Developer</h3>
-          <p class="Company">Brainstorm Technologies</p>
-        </a>
-        <a href="https://jobs.krisp.ai/3f8a35e3a4c24addaaccfecfdf24b57d" target="_blank">
-            <h3 class="Vacancy">Senior Backend Engineer II</h3>
-            <p class="Company">Krisp</p>
-        </a>
-        <a href="https://cauldron.app/jobs/60afa51c5ec67a4bb266ed03?c=1" target="_blank">
-            <h3 class="Vacancy">Node.js Developer</h3>
-            <p class="Company">CoinStats</p>
-        </a>
-        <a href="https://cauldron.app/jobs/60afa8685ec67a4bb266ed0d?c=1" target="_blank">
-            <h3 class="Vacancy">React.js Developer</h3>
-            <p class="Company">CoinStats</p>
-        </a>
-        <a href="https://fluxtech.me/careers/?tab=angular-developer" target="_blank">
-            <h3 class="Vacancy">Angular 2.X+ Developer</h3>
-            <p class="Company">Flux Technologies</p>
         </a>
         <a href="https://boards.greenhouse.io/vineti/jobs/4432956003" target="_blank">
             <h3 class="Vacancy">Application Support Engineer</h3>
@@ -143,6 +169,54 @@
         <a href="https://jobs.krisp.ai/4dbaf677237945ec9df4f52f419e3e3b" target="_blank">
             <h3 class="Vacancy">Fullstack Engineer II</h3>
             <p class="Company">Krisp</p>
+        </a>
+        <a href="https://jobs.krisp.ai/83244333ba154985b5d18c851ca0b979" target="_blank">
+            <h3 class="Vacancy">Application Support Engineer</h3>
+            <p class="Company">Krisp</p>
+        </a>
+        <a href="https://jobs.krisp.ai/f00548600b3e454e8bf8ff1089faede8" target="_blank">
+            <h3 class="Vacancy">Web Architect</h3>
+            <p class="Company">Krisp</p>
+        </a>
+        <a href="https://fluxtech.me/careers/?tab=senior-java-script-developer" target="_blank">
+            <h3 class="Vacancy">Lead JS Developer</h3>
+            <p class="Company">Flux Technologies</p>
+        </a>
+        <a href="https://sololearn.bamboohr.com/jobs/view.php?id=38" target="_blank">
+            <h3 class="Vacancy">Web Platform Lead</h3>
+            <p class="Company">Sololearn</p>
+        </a>
+        <a href="https://sololearn.bamboohr.com/jobs/view.php?id=52" target="_blank">
+            <h3 class="Vacancy">JavaScript Engineer</h3>
+            <p class="Company">Sololearn</p>
+        </a>
+        <a href="https://sololearn.bamboohr.com/jobs/view.php?id=51" target="_blank">
+            <h3 class="Vacancy">Senior JavaScript Engineer</h3>
+            <p class="Company">Sololearn</p>
+        </a>
+        <a href="https://jobs.dou.ua/companies/s-pro/vacancies/161934/" target="_blank">
+            <h3 class="Vacancy">Full-stack (React+Node.js) Developer (Remote)</h3>
+            <p class="Company">S-Pro</p>
+        </a>
+        <a href="https://www.epam.com/careers/job-listings/job.45413.senior-full-stack-javascript-engineer_yerevan_armenia" target="_blank">
+            <h3 class="Vacancy">Senior Full Stack JavaScript Engineer</h3>
+            <p class="Company">EPAM Systems</p>
+        </a>
+        <a href="https://staff.am/en/lead-javascript-developer" target="_blank">
+            <h3 class="Vacancy">Lead JavaScript Developer</h3>
+            <p class="Company">EPAM Systems</p>
+        </a>
+        <a href="https://www.epam.com/careers/job-listings/job.62181.remote_solution-architect-lead-javascript-developer" target="_blank">
+            <h3 class="Vacancy">Solution Architect</h3>
+            <p class="Company">EPAM Systems</p>
+        </a>
+        <a href="https://www.epam.com/careers/job-listings/job.56979.senior-javascript-developer_yerevan_armenia" target="_blank">
+            <h3 class="Vacancy">Senior JavaScript Developer</h3>
+            <p class="Company">EPAM Systems</p>
+        </a>
+        <a href="https://www.epam.com/careers/job-listings/job.55603.javascript-engineer_yerevan_armenia" target="_blank">
+            <h3 class="Vacancy">JavaScript Developer</h3>
+            <p class="Company">EPAM Systems</p>
         </a>
       </div>
     </div>
