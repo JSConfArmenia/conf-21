dfec518dd07a99792f06744b1f58152088cbc0b1
Varser <varserzakaryan@gmail.com>
2021-10-12 20:35:46 +0400

    typo fix

--- src/_services/speakers.js
+++ src/_services/speakers.js
@@ -22,7 +22,7 @@ export default {
   sandro: {
     img: '/speakers/sandro.jpg',
     name: 'Sandro Dolidze',
-    position: 'Co-founder & CTO at Tami Cloud',
+    position: 'Co-founder & CTO at Temi Cloud',
   },
   nairi: {
     img: '/speakers/nairi.jpg',
